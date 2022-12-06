d073bbaa5556fd1dcf9491e5697de22cf6d954a6
azure-sdk <azuresdk@microsoft.com>
2022-12-06 18:16:14 +0000

    Synchronize repo from Repoman

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -21,6 +21,10 @@ test("Create and delete item test", async ({ page }) => {
   await expect(page.locator(`text=${guid}`).first()).toBeVisible()
 
   await page.locator(`text=${guid}`).click();
+  const itemMoreDeleteButton = await page.$('button[role="menuitem"]:has-text("")');
+  if(itemMoreDeleteButton){
+    await itemMoreDeleteButton.click();
+  };
   await page.locator('button[role="menuitem"]:has-text("Delete")').click();
 
   await expect(page.locator(`text=${guid}`).first()).toBeHidden()
